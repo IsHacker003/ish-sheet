d515f3f0982f4f9a7caaf68a8549bc087a49a99d
Ishayu Roy <83446126+ishayu-c@users.noreply.github.com>
2022-01-25 11:46:04 +0530

    Update firebase-init.js

--- contact/firebase-init.js
+++ contact/firebase-init.js
@@ -14,23 +14,14 @@
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
   var firebaseConfig = {
-
     apiKey: "AIzaSyDNfKkBzORPlYI6zk7qc3_ArU-z8eRbInc",
-
     authDomain: "ish-sheet-contact.firebaseapp.com",
-
     databaseURL: "https://ish-sheet-contact-default-rtdb.firebaseio.com",
-
     projectId: "ish-sheet-contact",
-
     storageBucket: "ish-sheet-contact.appspot.com",
-
     messagingSenderId: "427347449513",
-
     appId: "1:427347449513:web:9eb1d22274f006de437bb8",
-
     measurementId: "G-55X95DVG2P"
-
   };
 
 
