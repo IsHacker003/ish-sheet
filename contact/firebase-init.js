e7d045cfbd0fcec628787cff0b86f943473a8a79
Ishayu Roy <83446126+ishayu-c@users.noreply.github.com>
2022-01-25 11:43:48 +0530

    Create firebase-init.js

---
+++ contact/firebase-init.js
@@ -0,0 +1,38 @@
+// Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-app.js";
+
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.4/firebase-analytics.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+  var firebaseConfig = {
+
+    apiKey: "AIzaSyDNfKkBzORPlYI6zk7qc3_ArU-z8eRbInc",
+
+    authDomain: "ish-sheet-contact.firebaseapp.com",
+
+    databaseURL: "https://ish-sheet-contact-default-rtdb.firebaseio.com",
+
+    projectId: "ish-sheet-contact",
+
+    storageBucket: "ish-sheet-contact.appspot.com",
+
+    messagingSenderId: "427347449513",
+
+    appId: "1:427347449513:web:9eb1d22274f006de437bb8",
+
+    measurementId: "G-55X95DVG2P"
+
+  };
+
+
+  // Initialize Firebase
+ firebase.initializeApp(firebaseConfig);
